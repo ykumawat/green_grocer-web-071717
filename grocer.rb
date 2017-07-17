c4521fef1a9895ec4c3b565a97ab5e46343e0961
Yamini Kumawat <yaminikumawat94@gmail.com>
2017-07-17 01:34:32 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,65 @@
 def consolidate_cart(cart)
   # code here
+  cartkeys = {}
+
+  cart.each do |e|
+    count = 1
+    e.each do |key, value|
+      if cartkeys[key]
+        cartkeys[key][:count] += 1
+      else
+        cartkeys[key] = value
+        cartkeys[key][:count] = count
+      end
+    end
+  end
+  cartkeys
 end
 
 def apply_coupons(cart, coupons)
   # code here
+  coupons.each do |e|
+    item = e[:item]
+    discountitem = "#{item} W/COUPON"
+    if cart.key?(item) && cart[item][:count] >= e[:num]
+      cart[item][:count] = cart[item][:count] - e[:num]
+      if cart[discountitem]
+        cart[discountitem][:count] += 1
+      else
+        cart[discountitem] = {
+          price: e[:cost],
+          clearance: cart[item][:clearance],
+          count: 1
+        }
+      end
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
   # code here
+  cart.each do |key, value|
+    if value[:clearance]
+      value[:price] = value[:price] * 0.80
+      value[:price] = value[:price].round(2)
+    end
+  end
+  cart
 end
 
 def checkout(cart, coupons)
   # code here
+  newcart = consolidate_cart(cart)
+  newcart2 = apply_coupons(newcart, coupons)
+  newcart3 = apply_clearance(newcart2)
+  total = 0.0
+  newcart3.each do |key, value|
+    total += value[:price] * value[:count]
+  end
+
+  if total > 100
+    total = total * 0.9
+  end
+  total
 end
